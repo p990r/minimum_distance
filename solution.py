2c8c95d5d2e98d797f945bf750c07d332655d2e3
p990r <59016212+p990r@users.noreply.github.com>
2020-01-02 11:31:03 +0100

    Solution file

---
+++ solution.py
@@ -0,0 +1,18 @@
+def solution(A):
+    # write your code in Python 3.6
+    pass
+
+    if (len(A)<2):
+        return -2
+
+    A.sort()
+    minDist = A[1]-A[0]
+
+    for i in range(2,len(A)):
+        dist = A[i] - A[i-1] # no need to use abs() since A is sorted
+        if(dist<minDist):
+            minDist = dist
+
+    if(minDist>100000000):
+        return -1
+    return minDist
